ba005574fb846eb8e1a6084131057c86b79eca1c
Sparsh Gupta <smartygupta8090027007@gmail.com>
2025-05-04 16:22:15 +0530

    update esbuild config to target ESNext and specify Linux platform

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux',
+  }
 })
