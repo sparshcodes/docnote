16029de785ea01fbde3e1f498ffeb2a02288cbc6
Sparsh Gupta <smartygupta8090027007@gmail.com>
2025-05-09 19:49:59 +0530

    fixing CORS

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
+import path from 'path';
 
 // app config
-const app = express()
-const port = process.env.PORT || 4000 
-connectDB()
-connectCloudinary()
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
-  origin: ['https://docnote-4qzx.vercel.app', 'http://localhost:5173'],
+  origin: ['https://docnote-eight.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
-}))
+}));
 
+// Handle preflight requests
 app.options('*', cors());
 
 // api end point
-app.use('/api/admin', adminRouter)
-app.use('/api/doctor', doctorRouter)
-app.use('/api/user', userRouter)
+app.use('/api/admin', adminRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
-
 app.get('/', (req, res) => {
-  res.send('Api working...')
-})
+  res.send('Api working...');
+});
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
